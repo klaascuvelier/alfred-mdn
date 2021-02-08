5d06af6cb11b568d77edf67eaaccaa3e4ad98e24
Klaas Cuvelier <klaas@showpad.com>
2021-02-08 09:51:36 +0100

    🐛 fix: breaking MDN api result

--- index.js
+++ index.js
@@ -3,39 +3,39 @@
 const alfy = require("alfy");
 const mdnBase = "https://wiki.developer.mozilla.org/api/v1/search/en-US";
 
-alfy.fetch(`${mdnBase}?q=${alfy.input}`, { transform })
-    .then(results => {
-        const items = (results || []).map(result => {
-            const {title, excerpt, url} = result;
-            const subtitle = stripHtml(excerpt);
+alfy.fetch(`${mdnBase}?q=${alfy.input}`, { transform }).then((results) => {
+  const items = (results || []).map((result) => {
+    const { title, highlight, slug } = result;
+    const excerpt = highlight && highlight.body && highlight.body.length > $;
+    const subtitle = stripHtml(excerpt);
 
-            return {
-                title,
-                subtitle,
-                autoComplete: title,
-                arg: url,
-                quicklookurl: url
-            }
-        });
+    return {
+      title,
+      subtitle,
+      autoComplete: title,
+      arg: url,
+      quicklookurl: url,
+    };
+  });
 
-        // No results
-        if (items.length === 0 && false) {
-            const url = `${mdnBase}?q=${alfy.input}`;
+  // No results
+  if (items.length === 0 && false) {
+    const url = `${mdnBase}?q=${alfy.input}`;
 
-            items.push({
-                title: `Show all results for '${alfy.input}'`,
-                arg: url,
-                quicklookurl: url
-            });
-        }
-
-        alfy.output(items);
+    items.push({
+      title: `Show all results for '${alfy.input}'`,
+      arg: url,
+      quicklookurl: url,
     });
+  }
+
+  alfy.output(items);
+});
 
 function transform(body) {
-    return body.documents;
+  return body.documents;
 }
 
 function stripHtml(text) {
-    return text.replace(/<[^>]+>/g, '');
+  return text.replace(/<[^>]+>/g, "");
 }
