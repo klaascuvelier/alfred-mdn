369ec5fe307bf47a6e8569f9bae7f4d62c5d8933
Klaas Cuvelier <klaas@showpad.com>
2021-02-08 10:07:49 +0100

    🐛 fix: new urls

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
 "use strict";
 
 const alfy = require("alfy");
-const mdnBase = "https://wiki.developer.mozilla.org/api/v1/search/en-US";
+const mdnDocBase = "https://developer.mozilla.org";
 
-alfy.fetch(`${mdnBase}?q=${alfy.input}`, { transform }).then((results) => {
-  const items = (results || []).map((result) => {
-    const { title, highlight, slug } = result;
-    const excerpt =
-      highlight && highlight.body && highlight.body.length > 0
-        ? highlight.body[0]
-        : "";
-    const subtitle = stripHtml(excerpt);
+https: alfy
+  .fetch(`${mdnDocBase}/api/v1/search/en-US?q=${alfy.input}`, { transform })
+  .then((results) => {
+    const items = (results || []).map((result) => {
+      const { title, highlight, mdn_url } = result;
+      const excerpt =
+        highlight && highlight.body && highlight.body.length > 0
+          ? highlight.body[0]
+          : "";
+      const subtitle = stripHtml(excerpt);
+      const url = `${mdnDoc}${mdn_url}`;
 
-    return {
-      title,
-      subtitle,
-      autoComplete: title,
-      arg: url,
-      quicklookurl: url,
-    };
-  });
-
-  // No results
-  if (items.length === 0 && false) {
-    const url = `${mdnBase}?q=${alfy.input}`;
-
-    items.push({
-      title: `Show all results for '${alfy.input}'`,
-      arg: url,
-      quicklookurl: url,
+      return {
+        title,
+        subtitle,
+        autoComplete: title,
+        arg: url,
+        quicklookurl: url,
+      };
     });
-  }
 
-  alfy.output(items);
-});
+    // No results
+    if (items.length === 0) {
+      const url = `${mdnDocBase}/en-US/search?q=${alfy.input}`;
+
+      items.push({
+        title: `Show all results for '${alfy.input}'`,
+        arg: url,
+        quicklookurl: url,
+      });
+    }
+
+    alfy.output(items);
+  });
 
 function transform(body) {
   return body.documents;
